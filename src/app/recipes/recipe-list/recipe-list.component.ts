f124e59c35a8c872615c42a17d3deefc2b736c7b
lacey-e <buchiezim@gmail.com>
2023-01-14 10:40:16 +0100

    ccc

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'test desc', 'https://www.forkinthekitchen.com/wp-content/uploads/2022/01/220113.stir_.fry_.sauce-0216-683x1024.jpg')
+  ];
 
-}
+  constructor() { }
+  
+  ngOnInit() {   
+  }
+  
+  
+  }
+  
